c96585bbf46ab8061577c49709f3dd0908f81b22
Christian Roman <christian.roman@crompco.com>
2017-03-31 20:42:47 +0000

    Removed integer cast

--- Solver.php
+++ Solver.php
@@ -17,22 +17,22 @@ class Solver
 	private function solve(Tree $root)
 	{
 		if ( is_numeric($root->root)  ) {
-			return intval($root->root);
+			return $root->root;
 		}
 
 		switch ( $root->root ) {
-			case '+':
-				return self::solve($root->left) + self::solve($root->right);
-				break;
-			case '-':
-				return self::solve($root->left) - self::solve($root->right);
-				break;
-			case '*':
-				return self::solve($root->left) * self::solve($root->right);
-				break;
-			case '/':
-				return self::solve($root->left) / self::solve($root->right);
-				break;
+		case '+':
+			return self::solve($root->left) + self::solve($root->right);
+			break;
+		case '-':
+			return self::solve($root->left) - self::solve($root->right);
+			break;
+		case '*':
+			return self::solve($root->left) * self::solve($root->right);
+			break;
+		case '/':
+			return self::solve($root->left) / self::solve($root->right);
+			break;
 		}
 	}
 }
